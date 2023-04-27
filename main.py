84423dc04d1b13a67f45a3b9f21385ef0952736c
eric-owen <ejo.owen@icloud.com>
2023-04-27 13:35:47 -0400

    sets up basic request to retrieve entire page source

---
+++ main.py
@@ -0,0 +1,12 @@
+import requests
+import selectorlib
+
+URL = "http://programmer100.pythonanywhere.com/tours/"
+
+def scrape(url):
+    response = requests.get(url)
+    source = response.text
+    return source
+
+if __name__ == "__main__":
+    print(scrape(URL))
