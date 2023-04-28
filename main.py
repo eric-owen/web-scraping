606f808eaaa8dd5e7b47d93bf20984b190f0009e
eric-owen <ejo.owen@icloud.com>
2023-04-28 12:19:08 -0400

    creates db connection and sql statements

--- main.py
+++ main.py
@@ -1,19 +1,52 @@
 import requests
 import selectorlib
+import time
+import sqlite3
 
 URL = "http://programmer100.pythonanywhere.com/tours/"
+
+connection = sqlite3.connect('db.db')
+
 
 def scrape(url):
     response = requests.get(url)
     source = response.text
     return source
 
+
 def extract(source):
     extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
     value = extractor.extract(source)["tours"]
     return value
 
+
+def read(extracted):
+    row = extracted.split(",")
+    row = [item.strip() for item in row]
+    band, city, date = row
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM events WHERE band=? AND city=? AND date=?", (band, city, date))
+    rows = cursor.fetchall()
+
+    return rows
+
+
+def store(extracted):
+    row = extracted.split(",")
+    row = [item.strip() for item in row]
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO events VALUES(?,?,?)", row)
+    connection.commit()
+
+
 if __name__ == "__main__":
-    scraped = scrape(URL)
-    extracted = extract(scraped)
-    print(extracted)
+    while True:
+        scraped = scrape(URL)
+        extracted = extract(scraped)
+        print(extracted)
+
+        if extracted != "No upcoming tours":
+            row = read(extracted)
+            if not row:
+                store(extracted)
+        time.sleep(2)
